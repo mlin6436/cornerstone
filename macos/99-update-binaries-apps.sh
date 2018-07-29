1050e4514373ba5ad17a5bbcb2fa0cfe94282053
Meng Lin <mlin6436@gmail.com>
2018-07-29 20:57:23 +0100

    Update 99-update-binaries-apps.sh

--- macos/99-update-binaries-apps.sh
+++ macos/99-update-binaries-apps.sh
@@ -5,6 +5,6 @@ if test ! $(which brew); then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew update && brew upgrade && brew cleanup && brew cask cleanup
+brew update && brew upgrade && brew cask upgrade && brew cleanup && brew cask cleanup
 
 exit 0
