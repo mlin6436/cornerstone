f27a4874422d0672ad81673a0955887a65a606bf
Meng Lin <mlin6436@gmail.com>
2018-08-05 21:17:09 +0100

    Update 20-java-version-switch.sh

--- macos/20-java-version-switch.sh
+++ macos/20-java-version-switch.sh
@@ -16,24 +16,24 @@ unlinkjava () {
   sudo unlink /usr/bin/javap
 }
 
-if [ $1 == 7 ]
+if [ $1 == 10 ]
   then 
-    echo "Setting up Java 7"
+    echo "Setting up Java 10"
     unlinkjava
-    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home/bin/java /usr/bin/java
-    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home/bin/javac /usr/bin/javac
-    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home/bin/javadoc /usr/bin/javadoc
-    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home/bin/javah /usr/bin/javah
-    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home/bin/javap /usr/bin/javap
+    sudo ln -s /Library/Java/JavaVirtualMachines/jdk-10.0.2.jdk/Contents/Home/bin/java /usr/bin/java
+    sudo ln -s /Library/Java/JavaVirtualMachines/jdk-10.0.2.jdk/Contents/Home/bin/javac /usr/bin/javac
+    sudo ln -s /Library/Java/JavaVirtualMachines/jdk-10.0.2.jdk/Contents/Home/bin/javadoc /usr/bin/javadoc
+    sudo ln -s /Library/Java/JavaVirtualMachines/jdk-10.0.2.jdk/Contents/Home/bin/javah /usr/bin/javah
+    sudo ln -s /Library/Java/JavaVirtualMachines/jdk-10.0.2.jdk/Contents/Home/bin/javap /usr/bin/javap
 fi
 
 if [ $1 == 8 ]
   then
     echo "Setting up Java 8"
     unlinkjava
-    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/bin/java /usr/bin/java
-    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/bin/javac /usr/bin/javac
-    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/bin/javadoc /usr/bin/javadoc
-    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/bin/javah /usr/bin/javah
-    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/bin/javap /usr/bin/javap
+    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/bin/java /usr/bin/java
+    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/bin/javac /usr/bin/javac
+    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/bin/javadoc /usr/bin/javadoc
+    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/bin/javah /usr/bin/javah
+    sudo ln -s /Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home/bin/javap /usr/bin/javap
 fi
